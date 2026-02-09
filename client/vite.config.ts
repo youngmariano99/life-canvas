7098ef348aa969c894e052f2a2bf8156b6fb67ca
Mariano.Y <youngmariano99@gmail.com>
2026-02-08 23:25:17 -0300

    sistema PWA

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -16,6 +16,9 @@ export default defineConfig(({ mode }) => ({
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5000000,
+      },
       manifest: {
         name: 'Life Canvas',
         short_name: 'LifeCanvas',
